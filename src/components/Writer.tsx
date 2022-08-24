e0c41789006f6a226b6a6d4f7e2a3a5aa6c53508
James <james@realja.me>
2022-08-24 12:50:16 -0400

    Start

---
+++ src/components/Writer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from "styled-components";
+
+const Textbox = styled.textarea`
+    
+`
+
+type Props = {}
+
+const Writer = (props: Props) => {
+  return (
+    <textarea></textarea> 
+  )
+}
+
+export default Writer
