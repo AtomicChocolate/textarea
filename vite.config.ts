83b735e8d9a26f4f188b33f25001f9d89d599e39
James <james@realja.me>
2023-09-03 19:40:12 -0400

    try to fix deploying still..

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_URL || '/',
+  base: "/<REPO>/",
   plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
   test: {
     globals: true,
