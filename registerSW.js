b48b8cdae4e3dd658e7633cc9e8b368c03422706
James <james@realja.me>
2023-09-03 19:50:32 -0400

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/%3CREPO%3E/sw.js', { scope: '/%3CREPO%3E/' })})}
