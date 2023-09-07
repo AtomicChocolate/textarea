7258cd873a2409ab9629b7ed9a96bf01f864922e
James <james@realja.me>
2023-09-07 11:54:03 -0400

    uh

---
+++ textarea-svelte/src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Nested from "./Nested.svelte";
+</script>
+
+<!-- <style>
+  p {
+    color: goldenrod;
+    font-family: "Comic Sans MS", cursive;
+    font-size: 2em;
+  }
+</style> -->
+
+<main>
+  <p class="text-purple-800 font-serif text-4xl">This is a paragraph.</p>
+  <Nested />
+</main>
